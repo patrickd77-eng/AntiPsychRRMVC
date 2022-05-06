a13732575f76da53b541810e361a5a36dc1f1e4f
patrickd77-eng <patrick_Davis97@hotmail.co.uk>
2022-05-06 15:27:01 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AntiPsychRRMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
